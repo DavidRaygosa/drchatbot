f8a0d443aab24b777d2b5c79223bd364dc2f6d16
David Raygosa <david_raygosa@hotmail.com>
2020-08-10 01:19:54 -0500

    Heroku PORT added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var mongoose = require('mongoose');
 var app = require('./app');
 var port = 3700;
+app.set('port', process.env.PORT || port)
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb+srv://davidr97:Azullindo55!@cluster0.k1pej.gcp.mongodb.net/DrChatbot?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
@@ -11,7 +12,7 @@ mongoose.connect('mongodb+srv://davidr97:Azullindo55!@cluster0.k1pej.gcp.mongodb
 			() =>
 				{
 					console.log("Conexion a la base de datos establecida satisfactoriamente...");
-					app.listen(port, () => 
+					app.listen(app.get('port'), () => 
 					{
  						console.log("Servidor Corriendo Correctamente En la URL: localhost:3700");
 					});
